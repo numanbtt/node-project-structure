9bb01276e362785cd1dca477b14699e2412c8e01
Numan <numanbtt@gmail.com>
2024-08-23 01:02:13 +0500

    chore:direct run typescript setup_(2024-08-23)

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import app from './app.js'; // When using ES modules, you need to include the .js file extension in your imports, even if you're importing TypeScript files that will be compiled to JavaScript
-import config from './config/index.js';
+import app from "./app.js"; // When using ES modules, you need to include the .js file extension in your imports, even if you're importing TypeScript files that will be compiled to JavaScript
+import config from "./config/index.js";
 
 const port = config.port || 3000;
 
-app.listen({port},()=>{
-    console.log(`Express is listening at http://localhost:${port}`)
+app.listen({ port }, () => {
+	console.log(`Express is listening at http://localhost:${port}`);
 });
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
